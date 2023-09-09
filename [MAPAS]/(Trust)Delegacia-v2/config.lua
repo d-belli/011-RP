802e9d01cb31f8f537e699d267f2ac86ca89b2d7
Deco Belli <25347602+d-belli@users.noreply.github.com>
2023-09-09 20:01:16 -0300

    Adicionado mapa detran e delegacia

---
+++ [MAPAS]/(Trust)Delegacia-v2/config.lua
@@ -0,0 +1,33 @@
+--============================================================
+
+--Desenvolvido por Trust Store: https://discord.gg/ah7Dgt7B4b
+
+--============================================================
+
+license = {
+    ["Email"] = "deco_031@hotmail.com", -- Coloque o email do seu discord em "seuemail" mantendo as ""
+    ["Key"] = "EF9-4A4-017-74F-TRUST" -- Coloque a sua key de ativação
+}
+
+
+--Obs: Não sabe como ativar o mapa? de uma olhada em nossos tutoriais: https://youtube.com/playlist?list=PLyk7YT4YZ3CoE5J9W9Q_QvRSDpAo1lZgn
+
+--============================================================
+
+configurar = {
+
+
+aclpolicial = "Policial", -- Acl que terá permissao para acessar as areas restitas da delegacia
+
+letracelas = "K", -- Letra que irá abrir as celas
+
+pilaresacl = true, -- Pilares por acl na entrada do estacionamento (true = sim | false = nao)
+portaosub = true, -- Portão por acl no estacionamento do subsolo (true = sim | false = nao)
+portasacl = true, -- Portas por acl no interior da dp (true = sim | false = nao)
+portascelas = true, -- Portas das celas (true = sim | false = nao)
+ 
+iddp = 3976, -- ID do objeto da dp
+idint = 3975, -- ID do objeto do interior dp
+idvidro = 4192, -- ID do objeto dos vidros da dp
+idlod = 4232, -- ID do objeto lod
+}
---
+++ [MAPAS]/(Trust)Detran/config.lua
@@ -0,0 +1,31 @@
+--============================================================
+
+--Desenvolvido por Trust Store: https://discord.gg/ah7Dgt7B4b
+
+--============================================================
+
+license = {
+    ["Email"] = "deco_031@hotmail.com", -- Coloque o email do seu discord em "seuemail" mantendo as ""
+    ["Key"] = "EF9-4A4-017-74F-TRUST" -- Coloque a sua key de ativação
+}
+
+
+--Obs: Não sabe como ativar o mapa? de uma olhada em nossos tutoriais: https://youtube.com/playlist?list=PLyk7YT4YZ3CoE5J9W9Q_QvRSDpAo1lZgn
+
+--============================================================
+
+configurar = {
+
+protecaoacl = true, -- Proteção por acl na area do estacionamento (true = sim | false = não)
+
+acldet = {"Detran", "Detran2"}, -- Acls que pode acessar todas as areas da base
+
+tempoportao = 3000, -- Tempo que o portão vai demorar para fechar (em milisegundos)
+
+lod = true, -- Renderizar o detran de longas distancias (isso pode afetar o desempenho)
+
+--====== ANTI CONFLITO ======--
+
+iddetran = 17865, -- Id do Detran
+
+}
